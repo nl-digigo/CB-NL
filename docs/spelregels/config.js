5e71f87b97b509ea89caa4725e62306569e690eb
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2022-10-14 11:17:21 +0200

    docs: toevoegen Spelregels, Usecases

---
+++ docs/spelregels/config.js
@@ -0,0 +1,15 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "CB-NL/spelregels",
+  specType: "technische documentatie", // vrije tekst, kleine letter
+  imprint: "bimloket",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Elisabeth de Vries",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/CB-NL",
+  license: "undefined",
+};
---
+++ docs/usecases/h/1-inleiding.md
@@ -0,0 +1,13 @@
+## Inleiding
+
+Deze specficatie pakt het volgende op:
+
+- opsomming
+- van
+- elementen
+
+| kolom     | column |
+| --------- | ------ |
+| waarde    | value  |
+| vrij      | free   |
+| { .data } |
