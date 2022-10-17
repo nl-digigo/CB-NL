aaa2dafedd3a0648da12f08b5ae459f550230b17
Elisabeth de Vries <elisabeth.kloren@crow.nl>
2022-10-17 09:10:59 +0200

    toevoegen documentatie pilot asfaltverhardingen

---
+++ docs/pilotasfalt/config.js
@@ -0,0 +1,24 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "CB-NL/pilotasfalt",
+  specType: "technische documentatie", // vrije tekst, kleine letter
+  imprint: "bimloket",
+  autors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Berwich Sluer",
+    },
+    {
+      name: "Hans Schevers",
+    },
+  ],
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Elisabeth de Vries",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/CB-NL",
+  license: "cc-by",
+};
