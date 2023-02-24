ededaaddccc4af57705d6d6d0a39b463e406779c
ElisabethKloren <ElisabethKloren@users.noreply.github.com>
2023-02-24 08:59:30 +0000

    Deploying to bimloket.github.io from @
    Reason: After push by ElisabethKloren

---
+++ routekaartinteroperabiliteit/config.js
@@ -0,0 +1,15 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "routekaartinteroperabiliteit/routekaartbim",
+  specType: "Overzicht standaarden", // vrije tekst, kleine letter
+  imprint: "bimloket",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Elisabeth de Vries",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/routekaartbim",
+  license: "cc-by",
+};
